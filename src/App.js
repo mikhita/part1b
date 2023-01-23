2a6eaeb96457bb78fb447aca40183427ab01c9e3
mikhita <mikheilispiriani@yahoo.com>
2023-01-23 19:43:56 +0400

    I add props to the components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
+import Context from "./components/Context";
 import Header from "./components/Header";
+import Total from "./components/Total";
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header title={course} />
-      <p>
-        {parts[0].name} {parts[0].exercises}
-      </p>
-      <p>
-        {parts[1].name} {parts[1].exercises}
-      </p>
-      <p>
-        {parts[2].name} {parts[2].exercises}
-      </p>
+      <Header title={course.name} />
+      <div style={{ display: "flex" }}>
+        <Context part={course.parts[0].name} />{" "}
+        <Total exercise={course.parts[0].exercises} />
+      </div>
+      <div style={{ display: "flex" }}>
+        <Context part={course.parts[1].name} />{" "}
+        <Total exercise={course.parts[1].exercises} />
+      </div>
+      <div style={{ display: "flex" }}>
+        <Context part={course.parts[2].name} />{" "}
+        <Total exercise={course.parts[2].exercises} />
+      </div>
       <p>
         {"exercises total number is: "}{" "}
-        {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+        {course.parts[0].exercises +
+          course.parts[1].exercises +
+          course.parts[2].exercises}
       </p>
     </div>
   );
---
+++ src/components/Total.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Total = (props) => {
+  return (
+    <div>{props.exercise}</div>
+  )
+}
+
+export default Total
