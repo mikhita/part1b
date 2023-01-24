01dfb627a47b325f4f237c2f47e2856e9102c7de
mikhita <mikheilispiriani@yahoo.com>
2023-01-24 09:37:41 +0400

    I did tutorial with useState

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
-import Context from "./components/Context";
-import Header from "./components/Header";
-import Total from "./components/Total";
+import { useState } from "react";
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+  const [counter, setCounter] = useState(0);
 
   return (
     <div>
-      <Header title={course.name} />
-      <div style={{ display: "flex" }}>
-        <Context part={course.parts[0].name} />{" "}
-        <Total exercise={course.parts[0].exercises} />
-      </div>
-      <div style={{ display: "flex" }}>
-        <Context part={course.parts[1].name} />{" "}
-        <Total exercise={course.parts[1].exercises} />
-      </div>
-      <div style={{ display: "flex" }}>
-        <Context part={course.parts[2].name} />{" "}
-        <Total exercise={course.parts[2].exercises} />
-      </div>
-      <p>
-        {"exercises total number is: "}{" "}
-        {course.parts[0].exercises +
-          course.parts[1].exercises +
-          course.parts[2].exercises}
-      </p>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>plus</button>
+      <button onClick={() => setCounter(0)}>zero</button>
     </div>
   );
 };
