6abf411a76c3a87ef58e7342069db4a5b98b39f1
mikhita <mikheilispiriani@yahoo.com>
2023-01-24 10:19:03 +0400

    I learn how to make the hardest destructing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import { useState } from "react";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [click, setClick] = useState({
+    left: 0,
+    right: 0,
+  });
+
+  const handleLeftClick = () => {
+    const newClick = {
+      left: click.left + 1,
+      right: click.right,
+    };
+    setClick(newClick);
+  };
+
+  const handleClickRight = () => {
+    const newClick = {
+      right: click.right + 1,
+      left: click.left,
+    };
+    setClick(newClick);
+  };
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>plus</button>
-      <button onClick={() => setCounter(0)}>zero</button>
+      {click.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleClickRight}>right</button>
+      {click.right}
     </div>
   );
 };
