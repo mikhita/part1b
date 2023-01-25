a9dc50c8d07eb33949dbb9a3aeadc03e47003cf3
mikhita <mikheilispiriani@yahoo.com>
2023-01-26 02:38:43 +0400

    anecdotes step 1

--- src/App.js
+++ src/App.js
@@ -46,38 +46,28 @@ const Stasistics = ({ good, neutral, bad, all, average, positive }) => {
 };
 
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
 
+  const [selected, setSelected] = useState(0);
   const handleClick = () => {
-    setGood(good + 1);
-  };
-  const handleClick1 = () => {
-    setNeutral(neutral + 1);
-  };
-  const handleClick2 = () => {
-    setBad(bad + 1);
+    setSelected(() => {
+      return anecdotes[Math.floor(Math.random() * anecdotes.length)];
+    });
   };
 
-  const all = good + neutral + bad;
-  const average = (good - bad) / 2;
-  const positive = (good / all) * 100;
   return (
     <div>
-      <h1>Give Feedback</h1>
-      <button onClick={handleClick}>good</button>
-      <button onClick={handleClick1}>neutral</button>
-      <button onClick={handleClick2}>bad</button>
-      <h2>stasistics</h2>
-      <Stasistics
-        good={good}
-        neutral={neutral}
-        bad={bad}
-        all={all}
-        average={average}
-        positive={positive}
-      />
+      {selected}
+      <button onClick={handleClick}>randomAnecdot</button>
     </div>
   );
 };
